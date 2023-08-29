226bd688d2ca95a38c9b292bca7e6f2fb1868701
Tibault Damman <tibault.damman@basalte.be>
2023-08-29 16:21:22 +0200

    throw in unimplemented functions
    sorry, Avahi users! I may come back and implement this one day...

--- src/AvahiMDNSManager.cpp
+++ src/AvahiMDNSManager.cpp
@@ -865,11 +865,13 @@ void MDNSManager::registerAddress(MDNSService &service,
                                   ErrorCodeHandler async_result)
 {
     // TODO
+    throw std::logic_error("MDNSManager::registerAddress not yet implemented for Avahi");
 }
 
 void MDNSManager::unregisterAddress(MDNSService &service)
 {
     // TODO
+    throw std::logic_error("MDNSManager::unregisterAddress not yet implemented for Avahi");
 }
 
 void MDNSManager::registerService(MDNSService &service)
